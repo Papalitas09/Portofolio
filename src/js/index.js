d23f98fc80bc30b9c0d84e9bdd61ca720b6de175
Papalitas09 <miaw.raja@gmail.com>
2024-08-28 04:24:45 +0700

    efe

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+function toogle(){
+    let skill = document.getElementById('Skills')
+
+    if(skill.style.display === "none"){
+        skill.style.display = "flex"
+    } else {
+        skill.style.display = "none";
+    }
+}
